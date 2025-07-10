e378ad886e19d14e519b967d322d31dcc0136dde
Armon <armon.hadjimani@gmail.com>
2025-07-10 13:06:20 -0500

    removed looping

--- backend/application.py
+++ backend/application.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, after_this_request
 from tweet_analysis import runscripts
 import threading
-import time
 import os
 
 app = Flask(__name__)
@@ -18,15 +17,6 @@ def safe_runscripts():
         except Exception as e:
             print(f"Error during runscripts(): {e}")
 
-# Background thread
-def run_in_loop():
-    while True:
-        safe_runscripts()
-        time.sleep(60 * 10)  # gets stopped by Azure after 2 hours
-
-# Start background loop
-threading.Thread(target=run_in_loop, daemon=True).start()
-
 @app.route('/', methods=['GET'])
 def run():
     @after_this_request
@@ -37,7 +27,7 @@ def run():
     safe_runscripts()  # Lock prevents overlap
     return jsonify(message="Manual run triggered.")
 
-# 🚀 This is what Render needs:
+#  This is what Render needs:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
